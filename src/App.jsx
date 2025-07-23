529d0c64b817774dcf31f9a871c919fb16966903
daridev <darideveloper@gmail.com>
2025-07-22 18:10:05 -0600

    delete karina files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-// Assets
-
-
-// Libs
-import clsx from 'clsx';
-import { useState } from 'react'
-
-// Components
-import Formulario from './components/sections/Formulario';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className={clsx('p-6')}>
-           <Formulario />  
-    </div>
+      <h1>hello world</h1>
     </>
   )
 }
